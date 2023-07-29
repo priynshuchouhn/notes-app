b546c7e7e2e5007c0a16aac0b160e097b8feea36
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-07-29 23:47:40 +0530

    Add delete completed

--- index.js
+++ index.js
@@ -13,21 +13,49 @@ const NoteSchema = new mongoose.Schema({ note: String });
 
 const Note = mongoose.model("note", NoteSchema);
 
+const item1 = new Note({
+    note: "Welcome to Todoey!"
+});
+const item2 = new Note({
+    note: "Add your items here."
+});
+
+const defaultItem = [item1,item2];
+
 app.get("/" , async(req,res)=>{
     let notes = await Note.find({});
-    console.log(notes);
+    if(notes.length === 0){
+        Note.insertMany(defaultItem, (err)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("Items added successfully");
+        })
+        res.redirect("/");
+    }
     res.render("index.ejs",{
         notes: notes
     });
 })
-app.post("/" ,(req,res)=>{
+app.post("/" , async(req,res) => {
+   const itemName =  req.body.newItem;
+   const newItem = new Note({
+    note: itemName
+   });
+   newItem.save();
+   res.redirect('/');
+})
 
-    const notes = Note.find();
-    console.log(notes);
-    res.render("index.ejs",{
-        notes: notes
+app.post("/delete" ,(req,res) => {
+    const checkedItemId =  req.body.checkbox;
+    Note.findByIdAndDelete(checkedItemId, function(err){
+        if(err){
+            console.log(err)
+        }
+        console.log("item deleted successfully!");
     });
-})
+    res.redirect('/');
+ })
 
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`);
